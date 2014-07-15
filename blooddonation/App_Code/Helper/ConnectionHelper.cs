89e1486b484d1a3d2c48ec9223c92ab225fd75f8
sumitbajracharya <vajrasummit@hotmail.com>
2014-07-15 12:34:34 +0545

    appcode and webconfig

---
+++ blooddonation/App_Code/Helper/ConnectionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ConnectionHelper
+/// </summary>
+public class ConnectionHelper
+{
+	public ConnectionHelper()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
