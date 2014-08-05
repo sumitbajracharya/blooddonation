ade45292899af08b806501e7d6e1e0744b96108f
Mesub <me_subash1901@yahoo.com>
2014-08-05 12:46:07 +0545

    sfs

---
+++ blooddonation/App_Code/BLL/BLLUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BLLUser
+/// </summary>
+public class BLLUser
+{
+	public BLLUser()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
---
+++ blooddonation/App_Code/DAL/ConnectionSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ConnectionSetting
+/// </summary>
+public class ConnectionSetting
+{
+	public ConnectionSetting()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
