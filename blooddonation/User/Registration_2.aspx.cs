2b6eb5aa3bbfa1e4a8af3d716d4300def44f10c9
Bibek Chaudhary <Mail2bibek@gmail.com>
2014-08-07 14:39:15 +0545

    reg 2 and user profile

--- blooddonation/User/Registration_2.aspx.cs
+++ blooddonation/User/Registration_2.aspx.cs
@@ -11,4 +11,22 @@ public partial class User_Registration_2 : System.Web.UI.Page
     {
 
     }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        MemberInfo _member = new MemberInfo();
+        _member.PermanentAddress = txtPermanentAddress.Text;
+        _member.CurrentAddress = txtCurrentAddress.Text;
+        _member.BestTime = ddlBestTimeToContact.SelectedValue;
+        _member.DOB=;
+        _member.LastDonationDate=;
+        _member.MobileNo= txtPhoneNumber.Text;
+        _member.ProfilePicture= fuProfilePicture.FileName;
+        fuProfilePicture.PostedFile.SaveAs(Server.MapPath("../Assets/Images/users/" + _member.ProfilePicture));
+         _member.BloodDonationCardSnapshot= fuBloodGroupCard.FileName; 
+         fuBloodGroupCard.PostedFile.SaveAs(Server.MapPath("../Assets/Images/Cards/" + _member.BloodDonationCardSnapshot));
+
+
+
+        BLLUser.CreateUser2(_member);
+    }
 }
--- blooddonation/User/UserProfile.aspx.cs
+++ blooddonation/User/UserProfile.aspx.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 public partial class User_UserProfile : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["UserName"] != null)
+        {
+            string name = Session["UserName"].ToString();
 
+            string query = "select * from TblMembers where UserName=" + name;
+            SqlConnection Con = ConnectionHelper.GetConnection();
+
+            SqlCommand cmd = new SqlCommand(query, Con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            MemberInfo _member = new MemberInfo();
+        
+
+
+
+
+        }
     }
 }
