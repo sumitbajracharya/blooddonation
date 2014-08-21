7d296fd1f7f773eb767058064b99c8ce47e0f9a4
Bibek Chaudhary <Mail2bibek@gmail.com>
2014-08-21 12:57:24 +0545

    donar details added

--- blooddonation/App_Code/BLL/BLLUser.cs
+++ blooddonation/App_Code/BLL/BLLUser.cs
@@ -110,17 +110,60 @@ public class BLLUser
         }        
     }
 
-
-    // Get user from user Name
     public static MemberInfo GetMemberByUserName(string UserName)
     {
 
         MemberInfo _member = new MemberInfo();
+
+        String sp = "Usp_GetMemeberbyName";
         
-        String query = "select * from TblMembers where (UserName=" + UserName+")";
+
         SqlConnection Con = ConnectionHelper.GetConnection();
 
-        SqlCommand cmd = new SqlCommand(query, Con);
+        SqlCommand cmd = new SqlCommand(sp, Con);
+        cmd.Parameters.Add(new SqlParameter("@username", UserName));
+
+        using (SqlDataReader _reader = cmd.ExecuteReader())
+        {
+            _reader.Read();
+            _member.MemberId = int.Parse(_reader["DonarID"].ToString());
+            _member.FirstName = _reader["FirstName"].ToString();
+            _member.LastName = _reader["LastName"].ToString();
+            _member.UserName = _reader["UserName"].ToString();
+            _member.PermanentAddress = _reader[""].ToString();
+            _member.CurrentAddress = int.Parse(_reader["CurrentAddress"].ToString());
+            _member.DOB = Convert.ToDateTime(_reader["DOB"]);
+            _member.BloodGroupId = int.Parse(_reader["BloodGroupID"].ToString());
+            _member.Gender = _reader["Gender"].ToString();
+            _member.LastDonationDate = Convert.ToDateTime(_reader["LastDonationDate"]);
+            _member.BestTime = _reader["BestTime"].ToString();
+            _member.MobileNo = _reader["MobileNumber"].ToString();
+            _member.PhoneNo = _reader["PhoneNumber"].ToString();
+            _member.Email = _reader["Email"].ToString();
+            _member.BloodDonationCardSnapshot = _reader["BloodDonationCardSnapshots"].ToString();
+            _member.DonorStatus = bool.Parse(_reader["DonorStatus"].ToString());
+            _member.AccountStatus = bool.Parse(_reader["AccountStatus"].ToString());
+            _member.RoleId = bool.Parse(_reader["RoleID"].ToString());
+            _member.ProfilePicture = _reader["ProfilePicture"].ToString();
+        }
+        return _member;
+    }
+
+
+    // Get user from userID
+    public static MemberInfo GetMemberByUserID(int UserID)
+    {
+
+
+        MemberInfo _member = new MemberInfo();
+
+        String sp = "Usp_GetMemeberbyID";
+
+
+        SqlConnection Con = ConnectionHelper.GetConnection();
+
+        SqlCommand cmd = new SqlCommand(sp, Con);
+        cmd.Parameters.Add(new SqlParameter("@userid", UserID));
 
         using (SqlDataReader _reader = cmd.ExecuteReader())
         {
