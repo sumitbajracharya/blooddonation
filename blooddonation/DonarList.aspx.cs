65718bfd3c62445e2dfcbd87dde6e153ac00139d
Bibek Chaudhary <Mail2bibek@gmail.com>
2014-08-21 15:54:17 +0545

    jyuyg

--- blooddonation/DonarList.aspx.cs
+++ blooddonation/DonarList.aspx.cs
@@ -11,17 +11,28 @@ public partial class DonarList : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Request.QueryString["Location&Bloodgroup"] != null)
+        if (Request.QueryString["Location"] != null)
         {
+
             int _location = int.Parse(Request.QueryString["Location"]);
-            int _Bloodgroup = int.Parse(Request.QueryString["Bloodgroup"]);
+            int _Bloodgroup = int.Parse(Request.QueryString["BloodGroup"]);
+            
             LoadList(_location, _Bloodgroup);
+
         }
+        
     }
 
     protected void LoadList(int Location, int BloodGroup)
     {
-        rptrDonorList.DataSource = BLLUser.GetDonarByLocationAndBloodGroup(Location, BloodGroup);
-        rptrDonorList.DataBind();
+        try
+        {
+            rptrDonorList.DataSource = BLLUser.GetDonarByLocationAndBloodGroup(Location, BloodGroup);
+            rptrDonorList.DataBind();
+        }
+        catch(Exception ex)
+        {
+            LblMessage.Text = ex.Message;
+        }
     }
 }
